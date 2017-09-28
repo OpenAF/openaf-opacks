420df8ddd8f8f146a5c46091ce9d19ae23b1ad99
Nuno Aguiar <nmaguiar@gmail.com>
2017-09-28 03:48:28 +0100

    Kube: support for authenticated access

--- Kube/kube.js
+++ Kube/kube.js
@@ -1,35 +1,63 @@
-var Kube = function(aURL) {
+var Kube = function (aURL, aUser, aPass) {
 	plugin("HTTP");
+	ow.loadFormat();
 	this.url = aURL;
+	this.user = aUser;
+	this.pass = aPass;
 }
 
-Kube.prototype.exec = function(aNamespace, aPod, aCommand, aTimeout, doSH) {
+Kube.prototype.exec = function (aNamespace, aPod, aCommand, aTimeout, doSH) {
 	var h = new HTTP();
+	if (isDef(this.user)) h.login(this.user, this.pass);
 
 	var pre = (doSH) ? "command=/bin/sh&command=-c&" : "";
 	var cmd = pre + "stderr=true&command=" + aCommand.split(/ +/).join("&command=");
-	var session; var out = "";
+	var session;
+	var out = "";
+
 	var client = h.wsConnect(this.url.replace(/^http/i, "ws") + "/api/v1/namespaces/" + aNamespace + "/pods/" + aPod + "/exec?" + encodeURI(cmd).replace(/\+/g, "%2B"),
-		function(s) { session = s; },
-		function(type, payload) {
+		function (s) {
+			session = s;
+		},
+		function (type, payload) {
 			out += af.fromBytes2String(payload).substring(1);
 		},
-		function(cause) { throw cause; },
-		function(sC, reason) { if (sC != 1000 || reason != null) throw reason; else aTimeout = 0; }
+		function (cause) {
+			th = cause;
+			throw cause;
+		},
+		function (sC, reason) {
+			if (sC != 1000 || reason != null) {
+				th = reason;
+				throw reason;
+			} else aTimeout = 0;
+		},
+		undefined,
+		true
 	);
 
-        var start = now();
-        if (isUnDef(aTimeout)) aTimeout = 300000;
+	var start = now();
+	if (isUnDef(aTimeout)) aTimeout = 300000;
 	while (out.length < 1 && ((now() - start) < aTimeout)) {
-		sleep(100);
+		client.get();
+	}
+	var prevOut = out.length;
+	var prevTime = now();
+	while ((now() - prevTime) < 150) {
+		sleep(50);
+		if (out.length > prevOut) {
+			prevOut = out.length;
+			prevTime = now();
+		}
 	};
-
-	client.stop();
+	session.stop();
 	return out;
 }
 
-Kube.prototype.getNamespaces = function(full) {
+Kube.prototype.getNamespaces = function (full) {
 	var h = new HTTP();
+	if (isDef(this.user)) h.login(this.user, this.pass);
+
 	var res = JSON.parse(h.exec(this.url + "/api/v1/pods").response);
 
 	if (full)
@@ -38,16 +66,17 @@ Kube.prototype.getNamespaces = function(full) {
 		return Object.keys($stream(clone(res.items)).groupBy("metadata.namespace"));
 }
 
-Kube.prototype.getNames = function(aNamespace, full) {
+Kube.prototype.getNames = function (aNamespace, full) {
 	var h = new HTTP();
+	if (isDef(this.user)) h.login(this.user, this.pass);
+
 	var res = JSON.parse(h.exec(this.url + "/api/v1/pods").response);
 
 	if (full) {
 		return $from(res.items).equals("metadata.namespace", aNamespace).select();
 	} else {
-		return $from(res.items).equals("metadata.namespace", aNamespace).select(function(rr) {
+		return $from(res.items).equals("metadata.namespace", aNamespace).select(function (rr) {
 			return rr.metadata.name;
 		})
 	}
 }
-
