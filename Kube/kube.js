78bf5fa96328f328f7534766781886ed6f569d3c
Nuno Aguiar <nmaguiar@users.noreply.github.com>
2019-01-29 19:24:42 +0000

    Kube: update to use fabric8 kubernetes client

--- Kube/kube.js
+++ Kube/kube.js
@@ -4,12 +4,24 @@
  * Creates an instance to access a kubernetes (k8s) cluster on aURL. If defined, using aUser and aPass.
  * </odoc>
  */
-var Kube = function (aURL, aUser, aPass) {
+var Kube = function (aURL, aUser, aPass, aWSTimeout) {
 	plugin("HTTP");
 	ow.loadFormat();
 	this.url = aURL; 
 	this.user = aUser;
 	this.pass = aPass;
+	//loadExternalJars(getOPackPath("Kube") || ".");
+	aWSTimeout = _$(aWSTimeout).isNumber().default(5000);
+	loadExternalJars(".");
+	this.config = (new Packages.io.fabric8.kubernetes.client.ConfigBuilder())
+	              .withMasterUrl(this.url)
+	              .withUsername(Packages.openaf.AFCmdBase.afc.dIP(this.user))
+	              .withPassword(Packages.openaf.AFCmdBase.afc.dIP(this.pass))
+				  .withTrustCerts(true)
+				  .withWebsocketTimeout(aWSTimeout)
+	              .build();
+				  //.withOauthToken("eyJhbGciOiJSUzI1NiIsImtpZCI6IiJ9.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJrdWJlLXN5c3RlbSIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VjcmV0Lm5hbWUiOiJhZG1pbi11c2VyLXRva2VuLXI0bWI3Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQubmFtZSI6ImFkbWluLXVzZXIiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC51aWQiOiJhMTA5ZWM1MC0xZTVjLTExZTktYmRlMy0wYTFhZDM4ZmI2NjQiLCJzdWIiOiJzeXN0ZW06c2VydmljZWFjY291bnQ6a3ViZS1zeXN0ZW06YWRtaW4tdXNlciJ9.T71FTSAgIFNOfS0vGbFx4T-ZPwZEVZL1zmeQFN2XULY1NwDI8LqPVjqrUKC43qbEaXcSdZ7UmO2vMOx__3F__GqMqQAsxFaKZ5i_PtVNzAsjmzanhQWLPDtCVVuRxJuwo-OfmK_KxSM9TIu-03NGEzkG9H-wFDK3B1IZdNf6mNLDeyzYM-nJWzv6NVN_tJRZoGt4hhedulAV_KJZaN9UTAhFG10SfpekzWQfNYsn-xLvGBXitwloUNMzOtEnJoTGXmurHAX9f2-EZlAAyLUGadWe4EE-pCXEqyy3K2uJ912BQCbDD7Q1T58KMa2QHvP_28JHnzXB8g-ooYnle_zJ4")
+	this.client = new Packages.io.fabric8.kubernetes.client.DefaultKubernetesClient(this.config);
 };
 
 /**
@@ -20,7 +32,29 @@ var Kube = function (aURL, aUser, aPass) {
  * </odoc>
  */
 Kube.prototype.exec = function (aNamespace, aPod, aCommand, aTimeout, doSH) {
-	var h = new HTTP();
+	var stream = new java.io.ByteArrayOutputStream();
+
+	var pre = (doSH) ? ["/bin/sh", "-c"] : [];
+	if (!isArray(aCommand)) aCommand = aCommand.split(/ +/);
+	pre = pre.concat(aCommand);
+	var cmd = newJavaArray(java.lang.String, pre.length);
+	for(var ii in pre) { cmd[ii] = new java.lang.String(pre[ii]); }
+
+	var watch = this.client.pods()
+				.inNamespace(aNamespace)
+				.withName(aPod)
+				.writingOutput(stream)
+				.writingError(stream)
+				.exec(cmd);
+	watch.waitUntilReady();
+
+	stream.flush();
+	var res = stream.toString();
+	stream.close();
+    watch.close();
+	
+	return res;
+	/*var h = new HTTP();
 	if (isDef(this.user)) h.login(this.user, this.pass, undefined, this.url.replace(/^http/i, "ws"));
 
 	var pre = (doSH) ? "command=/bin/sh&command=-c&" : "";
@@ -67,7 +101,7 @@ Kube.prototype.exec = function (aNamespace, aPod, aCommand, aTimeout, doSH) {
 	session.close();
 	session.stop();
 	res.client.stop();
-	return out;
+	return out;*/
 };
 
 /**
