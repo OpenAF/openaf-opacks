0db5ff8b8a6a01615a60e2f9dd41abdc20903e47
Nuno Aguiar <nmaguiar@users.noreply.github.com>
2019-07-03 20:41:03 +0100

    added more SQS functions

--- AWS/aws.js
+++ AWS/aws.js
@@ -11,6 +11,21 @@ var AWS = function(aAccessKey, aSecretKey) {
    this.secretKey = aSecretKey;
 };
 
+AWS.prototype.convertArray2Attrs = function(aParameter, anArray) {
+   var res = {};
+   for(var ii in anArray) {
+      res[aParameter + "." + (Number(ii) + 1) + ".Id"] = ii;
+      for(var jj in anArray[ii]) {
+         if (isArray(anArray[ii][jj])) {
+            res = merge(res, this.convertArray2Attrs(aParameter + "."+ (Number(ii) + 1) + "." + jj, anArray[ii][jj]));
+         } else {
+            res[aParameter + "."+ (Number(ii) + 1) + "." + jj] = anArray[ii][jj];
+         }
+      }
+   }
+   return res;
+};
+
 AWS.prototype.__HmacSHA256 = function(data, key) {
    var alg = "HmacSHA256";
    if (isString(key)) key = (new java.lang.String(key)).getBytes("UTF-8");
@@ -118,22 +133,142 @@ AWS.prototype.postURLEncoded = function(aURL, aURI, aParams, aArgs, aService, aH
  * <odoc>
  * <key>AWS.SQS_Send(aQueueEndPoint, aRegion, aMessageBody, aMessageGroupId, aMessageDeduplicationId) : Object</key>
  * Tries to send aMessageBody with aMessageGroupId and aMessageDeduplicationId to aQueueEndPoint on a specific aRegion. Returns
- * the result from calling the API.
+ * the result from calling the API. Optionally: aMessageBody can be a map with groupId, deduplicationId and body or an array 
+ * of these maps up to 10 elements.
  * </odoc>
  */
 AWS.prototype.SQS_Send = function(aEndPoint, aRegion, aMessageBody, aMessageGroupdId, aMessageDeduplicationId) {
-   var aURL = "https://sqs." + aRegion + ".amazonaws.com/" + aEndPoint;
+   var aURL = "https://sqs." + aRegion + ".amazonaws.com/" + aEndPoint.replace(/^\/+/, "").replace(/\/+$/, "");
    var url = new java.net.URL(aURL);
    var aURI = String(url.getPath());
-   var aHost = String(urll.getHos());
+   var aHost = String(url.getHost());
+   var res;
 
-   return this.postURLEncoded(aURL, aURI, "", { 
-      Action: "SendMessage", 
-      MessageBody: aMessageBody, 
-      MessageGroupId: aMessageGroupdId, 
-      MessageDeduplicationId: aMessageDeduplicationId, 
+   if (isArray(aMessageBody)) {
+      if (aMessageBody.length > 10) throw "Can't send more than 10 messages at a time due to AWS restrictions.";
+      var msg = {
+         Action: "SendMessageBatch",
+         Version: "2012-11-05"
+      };
+      msg = merge(msg, this.convertArray2Attrs("SendMessageBatchRequestEntry", aMessageBody));
+      res = this.postURLEncoded(aURL, aURI, "", msg, "sqs", aHost, aRegion);
+   } else {
+      if (isMap(aMessageBody)) {
+         aMessageGroupId = aMessageBody.groupId;
+         aMessageDeduplicationId = aMessageBody.deduplicationId;
+         aMessageBody = aMessageBody.body;
+      }
+      res = this.postURLEncoded(aURL, aURI, "", { 
+         Action: "SendMessage", 
+         MessageBody: aMessageBody, 
+         MessageDeduplicationId: (aEndPoint.endsWith(".fifo") ? aMessageDeduplicationId : void 0), 
+         MessageGroupId: (aEndPoint.endsWith(".fifo") ? aMessageGroupdId : void 0), 
+         Version: "2012-11-05" 
+      }, "sqs", aHost, aRegion);
+   }
+
+   if (isString(res)) res = af.fromXML2Obj(res);
+   return res;
+};
+
+/**
+ * <odoc>
+ * <key>AWS.SQS_Receive(aQueueEndPoint, aRegion, aVisibilityTimeout, aWaitTimeSeconds) : Object</key>
+ * Tries to send receive a message from the aQueueEndPoint on region aRegion given aVisibilityTimeout in seconds. If a message is not immediatelly available it will wait
+ * for aWaitTimeSeconds. On FIFO queues, after receiving the message you should delete it after successfully processing it within the visibility timeout period.
+ * </odoc>
+ */
+AWS.prototype.SQS_Receive = function(aEndPoint, aRegion, aVisibilityTimeout, aWaitTimeSeconds) {
+   var aURL = "https://sqs." + aRegion + ".amazonaws.com/" + aEndPoint.replace(/^\//, "");
+   var url = new java.net.URL(aURL);
+   var aURI = String(url.getPath());
+   var aHost = String(url.getHost());
+
+   var res = this.postURLEncoded(aURL, aURI, "", { 
+      Action: "ReceiveMessage", 
+      QueueUrl: aURL,
+      Version: "2012-11-05",
+      VisibilityTimeout: aVisibilityTimeout,
+      WaitTimeSeconds: aWaitTimeSeconds
+   }, "sqs", aHost, aRegion);
+   
+   if (isString(res)) res = af.fromXML2Obj(res);
+   return res;
+};
+
+/**
+ * <odoc>
+ * <key>AWS.SQS_Delete(aQueueEndPoint, aRegion, aReceiptHandle) : Object</key>
+ * Tries to send delete a message from the aQueueEndPoint on region aRegion given aReceiptHandle. If aReceiptHandle is actually a ReceiveMessageResponse/Result it will 
+ * provide the aReceiptHandle from the message.
+ * </odoc>
+ */
+AWS.prototype.SQS_Delete = function(aEndPoint, aRegion, aReceiptHandle) {
+   var aURL = "https://sqs." + aRegion + ".amazonaws.com/" + aEndPoint.replace(/^\//, "");
+   var url = new java.net.URL(aURL);
+   var aURI = String(url.getPath());
+   var aHost = String(url.getHost());
+
+   if (isMap(aReceiptHandle) && 
+       isDef(aReceiptHandle.ReceiveMessageResponse) &&
+       isDef(aReceiptHandle.ReceiveMessageResponse.ReceiveMessageResult)) {
+         aReceiptHandle = aReceiptHandle.ReceiveMessageResponse.ReceiveMessageResult.Message.ReceiptHandle;
+   };
+
+   var res = this.postURLEncoded(aURL, aURI, "", { 
+      Action: "DeleteMessage", 
+      QueueUrl: aURL,
+      ReceiptHandle: aReceiptHandle,
       Version: "2012-11-05" 
    }, "sqs", aHost, aRegion);
+   
+   if (isString(res)) res = af.fromXML2Obj(res);
+   return res;
+};
+
+/**
+ * <odoc>
+ * <key>AWS.SQS_GetQueueAttributes(aQueueEndPoint, aRegion) : Object</key>
+ * Tries to obtain and return the attributes of a queue of aQueueEndPoint on region aRegion.
+ * </odoc>
+ */
+AWS.prototype.SQS_GetQueueAttributes = function(aEndPoint, aRegion) {
+   var aURL = "https://sqs." + aRegion + ".amazonaws.com/" + aEndPoint.replace(/^\//, "");
+   var url = new java.net.URL(aURL);
+   var aURI = String(url.getPath());
+   var aHost = String(url.getHost());
+
+   var res = this.postURLEncoded(aURL, aURI, "", { 
+      Action: "GetQueueAttributes", 
+      QueueUrl: aURL,
+      "AttributeName.1": "All",
+      Version: "2012-11-05" 
+   }, "sqs", aHost, aRegion);
+   
+   if (isString(res)) res = af.fromXML2Obj(res);
+   return res;
+};
+
+/**
+ * <odoc>
+ * <key>AWS.SQS_Purge(aQueueEndPoint, aRegion) : Object</key>
+ * Tries to purge all messages from  a queue of aQueueEndPoint on region aRegion.
+ * </odoc>
+ */
+AWS.prototype.SQS_Purge = function(aEndPoint, aRegion) {
+   var aURL = "https://sqs." + aRegion + ".amazonaws.com/" + aEndPoint.replace(/^\//, "");
+   var url = new java.net.URL(aURL);
+   var aURI = String(url.getPath());
+   var aHost = String(url.getHost());
+
+   var res = this.postURLEncoded(aURL, aURI, "", { 
+      Action: "PurgeQueue", 
+      QueueUrl: aURL,
+      Version: "2012-11-05" 
+   }, "sqs", aHost, aRegion);
+   
+   if (isString(res)) res = af.fromXML2Obj(res);
+   return res;
 };
 
 /**
