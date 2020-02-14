0617b58a9dcbd9858f37cae3b48054ef90b4f263
Nuno Aguiar <nmaguiar@users.noreply.github.com>
2020-02-14 01:01:49 +0000

    AWS: added CLOUDWATCH_PutMetricData

--- AWS/aws_cloudwatch.js
+++ AWS/aws_cloudwatch.js
@@ -28,9 +28,48 @@ AWS.prototype.CLOUDWATCH_LOGS_DescribeLogGroups = function(aRegion, aLimit, aPre
        "X-Amz-Target": "Logs_20140328.DescribeLogGroups"
     }, void 0, "application/x-amz-json-1.1");
  
-    return res;
+    if (isMap(res))
+      return af.fromXML2Obj(res.error.response);
+    else
+      return af.fromXML2Obj(res);
  }; 
- 
+
+ /**
+  * <odoc>
+  * <key>AWS.CLOUDWATCH_PutMetricData(aRegion, aNamespace, aMetricDataArray) : Map</key>
+  * Tries to send aMetricDataArray of maps (up to 10 elements) under custom aNamespace in aRegion. 
+  * On the aMetricDataArray each map can contain:\
+  * \
+  *    MetricName: String\
+  *    Timestamp : NumberOfMilliseconds in UTC\
+  *    Unit      : [Seconds | Microseconds | Milliseconds | Bytes | Kilobytes | Megabytes | Gigabytes | Terabytes | Bits | Kilobits | Megabits | Gigabits | Terabits | Percent | Count | Bytes/Second | Kilobytes/Second | Megabytes/Second | Gigabytes/Second | Terabytes/Second | Bits/Second | Kilobits/Second | Megabits/Second | Gigabits/Second | Terabits/Second | Count/Second | None]\
+  *    Value     : aValue\
+  * \
+  * Check more in https://docs.aws.amazon.com/AmazonCloudWatch/latest/APIReference/API_MetricDatum.html.
+  * </odoc>
+  */
+ AWS.prototype.CLOUDWATCH_PutMetricData = function(aRegion, aNamespace, aMetricDat) {
+   aRegion = _$(aRegion).isString().default(this.region);
+   aNamespace = _$(aNamespace).isString().default("");
+   aMetricDat = _$(aMetricDat).isArray().default([]);
+
+   var aURL = "https://monitoring." + aRegion + ".amazonaws.com/";
+   var url = new java.net.URL(aURL);
+   var aHost = String(url.getHost());
+   var aURI = String(url.getPath());
+
+   var res = this.postURLEncoded(aURL, aURI, "", merge(this.convertArray2Attrs("MetricData.member", aMetricDat), {
+      Action   : "PutMetricData",
+      Version  : "2010-08-01",
+      Namespace: aNamespace
+   }), "monitoring", aHost, aRegion);
+
+   if (isMap(res))
+      return af.fromXML2Obj(res.error.response);
+   else
+      return af.fromXML2Obj(res);
+ };
+
  /**
   * <odoc>
   * <key>AWS.CLOUDWATCH_LOGS_DescribeLogStreams(aRegion, aGroupName, aLimit, aPrefix, aDescending, aOrderBy, nextToken) : Map</key>
@@ -60,7 +99,10 @@ AWS.prototype.CLOUDWATCH_LOGS_DescribeLogGroups = function(aRegion, aLimit, aPre
        "X-Amz-Target": "Logs_20140328.DescribeLogStreams"
     }, void 0, "application/x-amz-json-1.1");
  
-    return res;
+    if (isMap(res))
+      return af.fromXML2Obj(res.error.response);
+    else
+      return af.fromXML2Obj(res);
  };
  
  /**
@@ -96,7 +138,10 @@ AWS.prototype.CLOUDWATCH_LOGS_DescribeLogGroups = function(aRegion, aLimit, aPre
        "X-Amz-Target": "Logs_20140328.GetLogEvents"
     }, void 0, "application/x-amz-json-1.1");
  
-    return res;
+    if (isMap(res))
+      return af.fromXML2Obj(res.error.response);
+    else
+      return af.fromXML2Obj(res);
  };
  
  
