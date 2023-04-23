7d2f7f62dff074d7d70799f58dfaaaf95fb5aa6c
nmaguiar <nmaguiar@users.noreply.github.com>
2023-04-23 18:14:36 +0000

    GoogleCompiler: support new versions in java 11

--- GoogleCompiler/main.js
+++ GoogleCompiler/main.js
@@ -4,12 +4,19 @@ if (__expr.match(/--update/)) {
         throw "Not supported temporarially.";
 }
 
-if (typeof classPath === 'undefined') {
-	classPath = getOPackPath("GoogleCompiler") + "/compiler.jar";
+if (isUnDef(classPath)) {
+	var majorVersion = ow.format.getJavaVersion().match(/^\d+/)[0]
+	var jarFile
+
+	if (majorVersion < 11) jarFile = "compiler.jar"; else jarFile = "compiler11.jar"
+	classPath = getOPackPath("GoogleCompiler") + "/" + jarFile;
 	classPath = classPath.replace(/\\/g, "/");
 }
 
 var javaHome  = java.lang.System.getProperty("java.home") + "";
 var parameters = __expr.replace(/^exec GoogleCompiler/i, "");
-af.sh(javaHome + "/bin/java -jar " + classPath + "  " + parameters, "", null, true);
-if (__exitcode != 0) printErr(__stderr);
+var res = $sh(javaHome + "/bin/java -jar " + classPath + "  " + parameters).exec(0) 
+
+if (res.exitcode != 0) {
+	yprint(res)
+}
