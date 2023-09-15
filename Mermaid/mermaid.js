a73c5a558546db85e6421bd63275bf017a0c13c8
nmaguiar <nmaguiar@users.noreply.github.com>
2023-09-15 02:22:25 +0000

    bump mermaid

--- Mermaid/mermaid.js
+++ Mermaid/mermaid.js
@@ -6,9 +6,11 @@ ow.server.httpd.customLibs["mermaid.js"] = function(aHTTPd) {
     return aHTTPd.reply(io.readFileString(path + "/lib/mermaid.min.js"), ow.server.httpd.mimes.JS, ow.server.httpd.codes.OK, ow.server.httpd.cache.public)
 }
 
-const htmlExtras = [
+let htmlExtras = [
     "<script src=\"/js/mermaid.js\"></script>",
     "<script>mermaid.initialize({ startOnLoad: true })</script>"
 ].forEach(l => {
     if (ow.template.__mdHTMLExtras.indexOf(l) < 0) ow.template.__mdHTMLExtras.push(l)
 })
+
+if (isDef(ow.template.__srcPath)) ow.template.__srcPath["/js/mermaid.js"] = (getOPackPath("Mermaid") || ".") + "/lib/mermaid.min.js"
