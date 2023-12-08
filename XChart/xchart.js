95ed4eae7f34b4f086bc0f5579cc1c67d37bf25d
nmaguiar <nmaguiar@users.noreply.github.com>
2023-12-08 15:46:36 +0000

    bump xchart

--- XChart/xchart.js
+++ XChart/xchart.js
@@ -1,7 +1,8 @@
 ow.loadFormat();
 var OPACK_PATH = getOPackPaths()["XChart"] || ".";
 
-af.externalAddClasspath((new java.io.File(OPACK_PATH + "/lib/xchart-3.8.5.jar")).toURL());
+//af.externalAddClasspath((new java.io.File(OPACK_PATH + "/lib/xchart-3.8.6.jar")).toURL());
+loadExternalJars(OPACK_PATH + "/lib")
 
 /**
  * <odoc>
