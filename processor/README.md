d13ebceeb5f733807228b494f06f6563c5ab51e4
nmaguiar <nmaguiar@users.noreply.github.com>
2024-01-27 10:53:31 +0000

    +processor opack

---
+++ processor/README.md
@@ -0,0 +1,115 @@
+# OpenAF parser
+    
+Usage: _oafp [options]_
+
+## Main options:
+
+| Option | Description | 
+|--------|-------------|
+| -h     | Show this document |
+| file   | The file to parse (if not provide stdin is used) |
+| output | The output format (default: ctree) |
+| input  | The input type (if not provided it will try to be auto-detected) |
+| from   | An OpenAF nLinq path expression to filter output |
+| sql    | A SQL expression to filter output |
+| path   | A JMESPath expression to filter output |
+| csv    | If type=csv, the CSV options to use |
+| pause  | If 'true' will try to pause contents in alternative to _less -r_ |
+
+---
+
+## ⬆️ Output formats
+
+List of available formats to use with the _output_ option:
+
+| Output format | Description |
+|--------|-------------|
+| ctree  | A tree-like forcely colored format |
+| cjson  | A JSON forcely colored format |
+| ctable | A table-like forcely colored format (only for list outputs) |
+| tree   | A tree-like format |
+| json   | A JSON format without spacing |
+| prettyjson | A JSON format with spacing |
+| yaml   | A YAML format |
+| stable | A table-like format with word-wrap (only for list outputs) |
+| table  | A table-like format without size constraints (only for list outputs) |
+| xml    | An XML format |
+| ndjson | A NDJSON format |
+| cslon  | A SLON format forcely colored |
+| slon   | A SLON format |
+| csv    | A CSV format (only for list outputs) |
+| map    | A rectangle map format |
+| html   | An HTML format |
+| md     | A Markdown format |
+
+---
+
+## ⬇️  Input types
+
+List of types to use with the _input_ option:
+
+| Input type   | Description |
+|--------|-------------|
+| json   | A JSON format (auto-detected) |
+| yaml   | A YAML format (auto-detected) |
+| xml    | An XML format (auto-detected) |
+| csv    | A CSV format (auto-detected) |
+| ndjson | A NDJSON format |
+| md     | A Markdown format |
+
+---
+
+## 🧾 CSV options
+
+List of options to use with the _csv_ option (expects json or slon):
+
+| Option | Type | Description |
+|--------|------|-------------|
+| format | String | You can choose between DEFAULT, EXCEL, INFORMIX_UNLOAD, INFORMIX_UNLOAD_CSV, MYSQL, RFC4180, ORACLE, POSTGRESQL_CSV, POSTGRESQL_TEXT and TDF |
+| withHeader | Boolean | If true tries to automatically use the available header |
+| withHeaders | Array | A list of headers to use with the corresponding order |
+| quoteMode | String | You can choose between ALL, ALL_NON_NULL, MINIMAL, NON_NUMERIC and NONE. |
+| withDelimiter | String | A single character as a custom delimiter  |
+| withEscape | String | A single character as a custom escape |
+| withNullString | String | String to use as representation of null values |
+
+---
+
+## 🧾 XML options
+
+List of options to use when _output=xml_:
+
+| Option | Type | Description |
+|--------|------|-------------|
+| xmlignored | String | A comma-separated list of XML tags to ignore |
+| xmlprefix | String | A prefix to add to all XML tags |
+| xmlfiltertag | Boolean | If true will filter the XML tags |
+
+---
+
+## 📝 Examples
+
+```bash
+# simple processing through pipe
+cat someJsonFile.json | oafp
+
+# simple processing through pipe with scrolling
+cat someJsonFile.json | oafp output=ctree | less -r
+
+# specifying the input type and output format
+cat data.ndjson | oafp input=ndjson output=cslon
+``````
+
+```bash
+# markdown parsing of a file
+oafp file=someFile.md input=md
+
+# table with the latest news from Google
+curl -L https://blog.google/rss | oafp path="rss.channel.item" sql="select title, pubDate" output=ctable
+
+# table with the number of people in space per space craft
+curl http://api.open-notify.org/astros.json | oafp path="people" sql="select \"craft\", count(1) \"people\" group by \"craft\"" output=ctable
+
+# HTML with the list of near-earth asteroids and comets
+curl https://data.nasa.gov/resource/2vr3-k9wn.json | oafp output=html
+```
---
+++ processor/oafp.js
@@ -0,0 +1,135 @@
+#!/usr/bin/env /Applications/OpenAF/oaf-sb
+
+var params = processExpr(" ");
+// sprint(params)
+
+// var params = processExpr(" ")
+// Author : Nuno Aguiar
+var showHelp = () => {
+    __initializeCon()
+
+    var _f = (getOPackPath("processor") || ".") + "/README.md"
+    if (io.fileExists(_f)) {
+        __ansiColorFlag = true
+		__conConsole = true
+        if (isDef(ow.format.string.pauseString))
+            ow.format.string.pauseString( ow.format.withMD( io.readFileString(_f) ) )
+        else
+            print(ow.format.withMD( io.readFileString(_f) ))
+    } else {
+        print("Check https://github.com/OpenAF/openaf-opacks/blob/master/processor/README.md")
+    }
+
+    exit(0)
+}
+
+ow.loadFormat()
+if (__expr.indexOf(" -h") > -1) showHelp()
+
+var _file = params.file, _format = params.output || params.format, _type = params.input || params.type, _from = params.from, _sql = params.sql, _path = params.path, _csv = params.csv, _pause = params.pause
+var _xmlignored = params.xmlignored, _xmlprefix = params.xmlprefix, _xmlfiltertag = params.xmlfiltertag
+var _ndjsonjoin = params.ndjsonjoin
+
+// Default format
+_format = _$(_format, "format").isString().default("ctree")
+
+// Initialize console detection
+__initializeCon()
+
+// Set options
+var options = { __format: _format, __from: _from, __sql: _sql, __path: _path, __csv: _csv, __pause: _pause }
+if (_type == "ndjson") {
+    _ndjsonjoin = toBoolean(_$(_ndjsonjoin, "ndjsonjoin").isString().default(__))
+}
+
+// Read input from stdin or file
+var _res = "", noFurtherOutput = false
+if (isDef(_file)) {
+    if (_type != "csv") _res = io.readFileString(_file)
+} else {
+    _res = []
+    io.pipeLn(r => {
+        if (_type == "ndjson" && !_ndjsonjoin) {
+            $o(jsonParse(r, __, __, true), options)
+            noFurtherOutput = true
+        } else {
+            _res.push(r)
+        }
+        return false
+    })
+    _res = _res.join('\n')
+}
+
+if (!noFurtherOutput) {
+    // Detect type if not provided
+    if (isUnDef(_type)) {
+        // File name based
+        if (isDef(_file)) {
+            switch(_file.substring(_file.lastIndexOf('.'))) {
+            case '.json': _type = "json"; break
+            case '.yaml': _type = "yaml"; break
+            case '.xml' : _type = "xml"; break
+            case '.csv' : _type = "csv"; break
+            }
+        }
+
+        // Content-based
+        if (isUnDef(_type)) {
+            if (_res.trim().startsWith("{") || _res.trim().startsWith("[")) {
+                _type = "json"
+            } else if (_res.trim().startsWith("<")) {
+                _type = "xml"
+            } else {
+                if (isString(_res) && _res.length > 0) {
+                    if (_res.substring(0, _res.indexOf('\n')).split(",").length > 1) {
+                        _type = "csv"
+                    } else if (_res.substring(0, _res.indexOf(': ') > 0)) {
+                        _type = "yaml"
+                    }
+                } else {
+                    printErr("Please provide the input type.")
+                    exit(-1)
+                }
+            }
+        }
+    }
+
+    // Determine input type
+    switch(_type) {
+    case 'yaml':
+        $o(af.fromYAML(_res), options)
+        break
+    case 'xml':
+        _xmlignored = _$(_xmlignored, "xmlignored").isString().default(__)
+        _xmlprefix = _$(_xmlprefix, "xmlprefix").isString().default(__)
+        _xmlfiltertag = toBoolean(_$(_xmlfiltertag, "xmlfiltertag").isString().default(__))
+        $o(af.fromXML2Obj(_res, _xmlignored, _xmlprefix, _xmlfiltertag), options)
+        break
+    case 'ndjson':
+        if (_ndjsonjoin) {
+            $o(_res.split('\n').map(e => jsonParse(e.trim(), __, __, true)), options)
+        } else {
+            io.readLinesNDJSON(af.fromString2InputStream(_res), r => {
+                $o(r, options)
+            })
+        }
+        break
+    case 'md':
+        __ansiColorFlag = true
+		__conConsole = true
+        print(ow.format.withMD(_res))
+        break
+    case 'csv':
+        if (isDef(_file)) {
+            var is = io.readFileStream(_file)
+            $o($csv().fromInStream(is).toOutArray(), options)
+            is.close()
+        } else {
+            $o($csv().fromInString( _res ).toOutArray(), options)
+        }
+        break
+    case 'json':
+    default:
+        $o(jsonParse(_res, __, __, true), options)
+    }
+}
