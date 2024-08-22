cf3c2c12d523d4b2043a73a03c706a040a172a17
nmaguiar <nmaguiar@users.noreply.github.com>
2024-08-22 01:31:16 +0100

    chore: Update AWS to include ECR_BatchGetImage and ECR_GetRepositoryPolicy

--- AWS/aws_ecr.js
+++ AWS/aws_ecr.js
@@ -107,6 +107,44 @@ AWS.prototype.ECR_DescribeRepositories = function(aRegion, params) {
 
 /**
  * <odoc>
+ * <key>AWS.ECR_GetRepositoryPolicy(aRegion, aRepoName) : Map</key>
+ * Get the repository policy for aRepoName on aRegion.\
+ * \
+ * Refer to: https://docs.aws.amazon.com/AmazonECR/latest/APIReference/API_GetRepositoryPolicy.html\
+ * \
+ * </odoc>
+ */
+AWS.prototype.ECR_BatchGetImage = function(aRegion, aRepoName, aImageTag, params) {
+   var aURL
+   aRegion   = _$(aRegion, "aRegion").isString().default(this.region)
+   params    = _$(params, "params").isMap().default({})
+   aRepoName = _$(aRepoName, "aRepoName").isString().$_()
+   aImageTag = _$(aImageTag, "aImageTag").isString().$_()
+
+   aURL = "https://ecr." + aRegion + ".amazonaws.com/"
+   var url = new java.net.URL(aURL)
+   var aHost = String(url.getHost())
+   var aURI = String(url.getPath())
+
+   params.repositoryName      = aRepoName
+   params.acceptMediaTypes    = [ 
+      "application/vnd.docker.distribution.manifest.v1+json",
+      "application/vnd.docker.distribution.manifest.v2+json",
+      "application/vnd.oci.image.manifest.v1+json" 
+   ]
+   params.imageIds            = [ { imageTag: aImageTag } ]
+
+   var _res = this.postURLEncoded(aURL, aURI, "", params, "ecr", aHost, aRegion, {
+      "X-Amz-Target": "AmazonEC2ContainerRegistry_V20150921.BatchGetImage"
+   }, __, "application/x-amz-json-1.1")
+
+   if (isDef(_res) && isArray(_res.images) && _res.images.length > 0) return _res.images[0]
+
+   return _res
+}
+
+/**
+ * <odoc>
  * <key>AWS.ECR_DescribeImageScanFindings(aRegion, aRepoName, aImageDigest, params) : Map</key>
  * List all scan findings for aImageDigest (from ECR_ListImages) from aRepoName on aRegion. Optionally you can provide extra params.
  * \
@@ -131,4 +169,31 @@ AWS.prototype.ECR_DescribeImageScanFindings = function(aRegion, aRepoName, aImag
     return this.postURLEncoded(aURL, aURI, "", params, "ecr", aHost, aRegion, {
        "X-Amz-Target": "AmazonEC2ContainerRegistry_V20150921.DescribeImageScanFindings"
     }, void 0, "application/x-amz-json-1.1");
+}
+
+/**
+ * <odoc>
+ * <key>AWS.ECR_GetRepositoryPolicy(aRegion, aRepoName) : Map</key>
+ * Get the repository policy for aRepoName on aRegion.\
+ * \
+ * Refer to: https://docs.aws.amazon.com/AmazonECR/latest/APIReference/API_GetRepositoryPolicy.html\
+ * \
+ * </odoc>
+ */
+AWS.prototype.ECR_GetAuthorizationToken = function(aRegion) {
+   var aURL
+   aRegion = _$(aRegion).isString().default(this.region)
+
+   aURL = "https://ecr." + aRegion + ".amazonaws.com/"
+   var url = new java.net.URL(aURL)
+   var aHost = String(url.getHost())
+   var aURI = String(url.getPath())
+
+   var _res = this.postURLEncoded(aURL, aURI, "", {}, "ecr", aHost, aRegion, {
+      "X-Amz-Target": "AmazonEC2ContainerRegistry_V20150921.GetAuthorizationToken"
+   }, __, "application/x-amz-json-1.1")
+
+   if (isDef(_res) && isDef(_res.authorizationData)) {
+      return _res.authorizationData[0]
+   }  
 }
