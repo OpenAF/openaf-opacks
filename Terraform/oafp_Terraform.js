49a73ca314332665b24bc85964bbd5d5629a25a3
Nuno Aguiar <nmaguiar@users.noreply.github.com>
2025-03-10 13:31:24 +0000

    terraform: add hcl references for oafp

--- Terraform/oafp_Terraform.js
+++ Terraform/oafp_Terraform.js
@@ -2,8 +2,14 @@
     loadLib("terraform.js")
     exports.oafplib = function(params, _$o, $o, oafp) {
         var _r = {
-            fileExtensions: [ { ext: ".tf", type: "tf" } ],
-            input         : [ { 
+            fileExtensions: [ { ext: ".tf", type: "tf" }, { ext: ".hcl", type: "hcl" } ],
+            input         : [ {
+                type: "hcl",
+                fn  : (r, options) => {
+                    oafp._showTmpMsg()
+                    var _tf = new Terraform()
+                    _$o(_tf.fromHCL(r), options)
+                },
                 type: "tf", 
                 fn: (r, options) => {
                     oafp._showTmpMsg()
@@ -13,6 +19,11 @@
             } ],
             output        : [ { 
                 type: "tf", 
+                fn: (r, options) => {
+                    var _tf = new Terraform()
+                    oafp._print(_tf.toHCL(r))
+                },
+                type: "hcl", 
                 fn: (r, options) => {
                     var _tf = new Terraform()
                     oafp._print(_tf.toHCL(r))
@@ -29,6 +40,7 @@ Extra input types added by the terraform lib:
 | Input type | Description |
 |------------|-------------|
 | tf       | Terraform/HCL input format  |
+| hcl      | HCL input format |
 
 ---
 
@@ -39,6 +51,7 @@ Extra output formats added by the terrform lib:
 | Output format | Description |
 |---------------|-------------|
 | tf          | Terraform/HCL output format  |
+| hcl         | HCL output format |
 `
         }
 
