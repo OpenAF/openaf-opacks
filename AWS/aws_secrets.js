797a6698ac05b0e01c57a367ec7733dc12fedfbc
nmaguiar <nmaguiar@users.noreply.github.com>
2025-03-13 15:40:16 +0000

    AWS: feat: Add AWS Secrets Manager functions for retrieving and updating secrets

--- AWS/aws_secrets.js
+++ AWS/aws_secrets.js
@@ -29,3 +29,56 @@ AWS.prototype.SECRETS_ListSecrets = function(aRegion) {
         return _r
     }
 }
+
+/**<
+ * <odoc>
+ * <key>AWS.SECRETS_GetSecret(aRegion, aSecretId) : Map</key>
+ * Given aRegion and aSecretId (ARN) returns a map with the secret information.
+ * </odoc>
+ */
+AWS.prototype.SECRETS_GetSecret = function(aRegion, aSecretId) {
+    aRegion = _$(aRegion).isString().default(this.region)
+    var aURL = "https://secretsmanager." + aRegion + ".amazonaws.com/"
+    
+    var params = {
+        SecretId: aSecretId
+    }
+ 
+    var url = new java.net.URL(aURL)
+    var aHost = String(url.getHost())
+    var aURI = String(url.getPath())
+ 
+    var _r = this.postURLEncoded(aURL, aURI, "", params, "secretsmanager", aHost, aRegion, {
+        "X-Amz-Target": "secretsmanager.GetSecretValue"
+    }, __, "application/x-amz-json-1.1")
+
+    return _r
+}
+
+/**
+ * <odoc>
+ * <key>AWS.SECRETS_PutSecretString(aRegion, aSecretId, aNewStringValue) : Map</key>
+ * Given aRegion, aSecretId (ARN) and aNewStringValue will update the secret with the new value.
+ * Note: a ClientRequestToken will be automatically generated.
+ * </odoc>
+ */
+AWS.prototype.SECRETS_PutSecretString = function(aRegion, aSecretId, aNewStringValue) {
+    aRegion = _$(aRegion).isString().default(this.region)
+    var aURL = "https://secretsmanager." + aRegion + ".amazonaws.com/"
+    
+    var params = {
+        ClientRequestToken: genUUID(),
+        SecretId    : aSecretId,
+        SecretString: aNewStringValue
+    }
+ 
+    var url = new java.net.URL(aURL)
+    var aHost = String(url.getHost())
+    var aURI = String(url.getPath())
+ 
+    var _r = this.postURLEncoded(aURL, aURI, "", params, "secretsmanager", aHost, aRegion, {
+        "X-Amz-Target": "secretsmanager.PutSecretValue"
+    }, __, "application/x-amz-json-1.1")
+
+    return _r
+}
