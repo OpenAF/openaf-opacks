7bb3f8a8d528249a985e169d37ffeb8d440f8cef
nmaguiar <nmaguiar@users.noreply.github.com>
2025-04-04 13:44:41 +0100

    AWS: enhance STS functionality with AssumeRole methods

--- AWS/aws_sts.js
+++ AWS/aws_sts.js
@@ -23,3 +23,52 @@ AWS.prototype.STS_GetCallerIdentity = function(aRegion) {
 
     if (isMap(res)) return res; else return af.fromXML2Obj(res)
 }
+
+/**
+ * <odoc>
+ * <key>AWS.STS_GetSessionToken(aRegion, aDurationSecs) : Map</key>
+ * Given a region and a duration in seconds, returns a map with the
+ * session token, access key and secret key.
+ * </odoc>
+ */
+AWS.prototype.STS_AssumeRole = function(aRegion, aRoleARN, aRoleSession, aDurationSecs) {
+    aRegion = _$(aRegion).isString().default(this.region)
+    aRoleARN = _$(aRoleARN, "aRoleARN").isString().$_()
+    aRoleSession = _$(aRoleSession, "aRoleSession").isString().default(genUUID())
+
+    var aURL = "https://sts." + aRegion + ".amazonaws.com/"
+    var url = new java.net.URL(aURL)
+    var aHost = String(url.getHost())
+    var aURI = String(url.getPath())
+    var params = {
+       Action: "AssumeRole",
+       Version: "2011-06-15",
+       RoleSessionName: aRoleSession,
+       RoleArn: aRoleARN,
+       DurationSeconds: aDurationSecs
+    }
+
+    var res = this.postURLEncoded(aURL, aURI, "", params, "sts", aHost, aRegion)
+
+    if (!isMap(res)) res = af.fromXML2Obj(res)
+    if (isDef(res.AssumeRoleResponse)) res = res.AssumeRoleResponse
+    return res
+}
+
+/**
+ * <odoc>
+ * <key>AWS.assumeRole(aRoleARN, aRoleSession, aDurationSecs) : AWS</key>
+ * Given a role ARN, a session name and a duration in seconds, returns
+ * an AWS object with the new credentials.
+ * </odoc>
+ */
+AWS.prototype.assumeRole = function(aRoleARN, aRoleSession, aDurationSecs) {
+    var o = this.STS_AssumeRole(this.region, aRoleARN, aRoleSession, aDurationSecs)
+    if (isDef(o.AssumeRoleResult)) o = o.AssumeRoleResult
+    if (isDef(o.Credentials)) o = o.Credentials
+    if (isDef(o.AccessKeyId)) {
+        return new AWS(o.AssumeRoleResult.Credentials.AccessKeyId, o.AssumeRoleResult.Credentials.SecretAccessKey, o.AssumeRoleResult.Credentials.SessionToken)
+    } else {
+        throw new Error("Invalid response from AWS STS AssumeRole: " + JSON.stringify(o))
+    }
+}
