f59e17ee1a0d9e3518bb27bbc972a698175893ac
nmaguiar <nmaguiar@users.noreply.github.com>
2026-08-21 17:18:56 +0100

    feat(S3): add extensive functionality to oafp_S3.js
    Updated oafp_S3.js with 203 insertions and 6 deletions adding new S3 processing capabilities. Updated .package.yaml metadata accordingly.

--- S3/oafp_S3.js
+++ S3/oafp_S3.js
@@ -32,14 +32,112 @@
                         s3.close()
                     }
                 }
+            }, {
+                type: "s3listbuckets",
+                fn  : (r, options) => {
+                    var s3 = new S3(params.s3url, params.s3accesskey, params.s3secretkey, params.s3region)
+                    try {
+                        _$o(s3.listBuckets(), options)
+                    } finally {
+                        s3.close()
+                    }
+                }
+            }, {
+                type: "s3listobjects",
+                fn  : (r, options) => {
+                    _$(params.s3bucket, "s3bucket").isString().$_()
+
+                    var s3 = new S3(params.s3url, params.s3accesskey, params.s3secretkey, params.s3region)
+                    try {
+                        _$o(s3.listObjects(params.s3bucket, params.s3prefix, toBoolean(params.s3full), toBoolean(params.s3recursive)), options)
+                    } finally {
+                        s3.close()
+                    }
+                }
+            }, {
+                type: "s3getobject",
+                fn  : (r, options) => {
+                    _$(params.s3bucket, "s3bucket").isString().$_()
+                    _$(params.s3object, "s3object").isString().$_()
+
+                    var s3offset = _$(params.s3offset).toNumber().isNumber().default(__)
+                    var s3length = _$(params.s3length).toNumber().isNumber().default(__)
+
+                    var s3 = new S3(params.s3url, params.s3accesskey, params.s3secretkey, params.s3region)
+                    try {
+                        var result = s3.getObjectStream(params.s3bucket, params.s3object, s3offset, s3length)
+                        try {
+                            _$o(af.fromBytes2String(af.fromInputStream2Bytes(result)), options)
+                        } finally {
+                            result.close()
+                        }
+                    } finally {
+                        s3.close()
+                    }
+                }
+            }, {
+                type: "s3statobject",
+                fn  : (r, options) => {
+                    _$(params.s3bucket, "s3bucket").isString().$_()
+                    _$(params.s3object, "s3object").isString().$_()
+
+                    var s3 = new S3(params.s3url, params.s3accesskey, params.s3secretkey, params.s3region)
+                    try {
+                        _$o(s3.statObject(params.s3bucket, params.s3object), options)
+                    } finally {
+                        s3.close()
+                    }
+                }
             } ],
 
-            output        : [ /*{
-                type: "test",
-                fn: (r, options) => {
-                    $o({ test: 'test output' }, options)
+            output        : [ {
+                type: "s3putobject",
+                fn  : (r, options) => {
+                    _$(params.s3bucket, "s3bucket").isString().$_()
+                    _$(params.s3object, "s3object").isString().$_()
+
+                    var content = isString(r) ? r : stringify(r, __, "")
+                    var s3 = new S3(params.s3url, params.s3accesskey, params.s3secretkey, params.s3region)
+                    try {
+                        var is = af.fromString2InputStream(content)
+                        try {
+                            s3.putObjectStream(params.s3bucket, params.s3object, is, __, params.s3contenttype)
+                        } finally {
+                            is.close()
+                        }
+                    } finally {
+                        s3.close()
+                    }
                 }
-            }*/ ],
+            }, {
+                type: "s3removeobject",
+                fn  : (r, options) => {
+                    _$(params.s3bucket, "s3bucket").isString().$_()
+                    var aObject = isDef(params.s3object) ? params.s3object : String(r)
+
+                    var s3 = new S3(params.s3url, params.s3accesskey, params.s3secretkey, params.s3region)
+                    try {
+                        s3.removeObject(params.s3bucket, aObject)
+                    } finally {
+                        s3.close()
+                    }
+                }
+            }, {
+                type: "s3copyobject",
+                fn  : (r, options) => {
+                    _$(params.s3bucket, "s3bucket").isString().$_()
+                    _$(params.s3object, "s3object").isString().$_()
+                    _$(params.s3targetbucket, "s3targetbucket").isString().$_()
+                    _$(params.s3targetobject, "s3targetobject").isString().$_()
+
+                    var s3 = new S3(params.s3url, params.s3accesskey, params.s3secretkey, params.s3region)
+                    try {
+                        s3.copyObject(params.s3bucket, params.s3object, params.s3targetbucket, params.s3targetobject)
+                    } finally {
+                        s3.close()
+                    }
+                }
+            } ],
             transform     : [ /*{
                 type: "test",
                 fn: (r) => {
@@ -56,6 +154,10 @@ Extra input types added by the S3 lib:
 | Input type | Description |
 |------------|-------------|
 | s3select   | Input data from running an S3 Select SQL expression against an object. |
+| s3listbuckets | Input data listing all the existing buckets. |
+| s3listobjects | Input data listing the objects in a bucket. |
+| s3getobject | Input data from retrieving the content of an object. |
+| s3statobject | Input data with the metadata of an object. |
 
 All S3 inputs have the following common options:
 
@@ -84,6 +186,101 @@ List of options to use when _in=s3select_:
 The input data will be taken as the S3 Select SQL expression to execute (e.g. \`SELECT s.name FROM S3Object s WHERE CAST(s.age AS INT) >= 18\`).
 
 Note: Parquet input is supported on AWS S3 but not on MinIO servers by default.
+
+---
+
+## 🧾 S3ListBuckets input options
+
+List of options to use when _in=s3listbuckets_:
+
+No extra options besides the common S3 input options above.
+
+---
+
+## 🧾 S3ListObjects input options
+
+List of options to use when _in=s3listobjects_:
+
+| Option | Type | Description |
+|--------|------|-------------|
+| s3bucket | String | The bucket to list objects from. |
+| s3prefix | String | An optional prefix to simulate listing a folder (optional). |
+| s3full | Boolean | If true the contentType will also be retrieved for each object (optional, slower, default false). |
+| s3recursive | Boolean | If true all "object paths" will be traversed recursively (optional, default false). |
+
+---
+
+## 🧾 S3GetObject input options
+
+List of options to use when _in=s3getobject_:
+
+| Option | Type | Description |
+|--------|------|-------------|
+| s3bucket | String | The bucket holding the object to retrieve. |
+| s3object | String | The object key to retrieve. |
+| s3offset | Number | An optional starting offset (in bytes) to read from. |
+| s3length | Number | An optional number of bytes to read (requires s3offset). |
+
+---
+
+## 🧾 S3StatObject input options
+
+List of options to use when _in=s3statobject_:
+
+| Option | Type | Description |
+|--------|------|-------------|
+| s3bucket | String | The bucket holding the object. |
+| s3object | String | The object key to stat. |
+
+---
+
+## ⬆️  S3 output types:
+
+Extra output types added by the S3 lib:
+
+| Output type | Description |
+|--------------|-------------|
+| s3putobject | Writes the output data as the content of an object. |
+| s3removeobject | Removes an object. |
+| s3copyobject | Copies an object to another bucket/object. |
+
+---
+
+## 🧾 S3PutObject output options
+
+List of options to use when _out=s3putobject_:
+
+| Option | Type | Description |
+|--------|------|-------------|
+| s3bucket | String | The bucket to put the object into. |
+| s3object | String | The object key to put. |
+| s3contenttype | String | An optional content type for the object. |
+
+The output data (converted to a string if needed) will be used as the object's content.
+
+---
+
+## 🧾 S3RemoveObject output options
+
+List of options to use when _out=s3removeobject_:
+
+| Option | Type | Description |
+|--------|------|-------------|
+| s3bucket | String | The bucket holding the object to remove. |
+| s3object | String | The object key to remove (optional; if not provided the output data is used as the object key). |
+
+---
+
+## 🧾 S3CopyObject output options
+
+List of options to use when _out=s3copyobject_:
+
+| Option | Type | Description |
+|--------|------|-------------|
+| s3bucket | String | The source bucket. |
+| s3object | String | The source object key. |
+| s3targetbucket | String | The target bucket. |
+| s3targetobject | String | The target object key. |
 
 `
         }
